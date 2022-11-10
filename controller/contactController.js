a52a95e3c3c7b0bd9636c5a23616c8ae964ea96d
shakti <shakti.ingenioushitech@gmail.com>
2022-11-10 21:33:15 +0530

    make it better

--- controller/contactController.js
+++ controller/contactController.js
@@ -4,15 +4,15 @@ module.exports.addContact = async (req, res) => {
     const { name ,email ,phoneNumber , message , subject } = req.body
     if ( name && email  && phoneNumber && message && subject) {
    try {
-    const doc = new contactModel({
+    const data = new contactModel({
       name: name,
       email: email,
       phoneNumber:phoneNumber,
       message:message,
       image:req?.file?.filename
             })
-            await doc.save()
-            res.status(201).send({ "status":"200", "success":true, "message": "Add contact  Successfully" })
+            await data.save()
+            res.status(201).send({ "status":"200", "success":true, "message": "Add contact  Successfully",data})
           } catch (error) {
             console.log(error)
             res.status(401).send({ "status": "401","success":false, "message": "Unable to contact" })
