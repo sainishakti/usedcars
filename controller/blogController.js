d8d0af84d36417d4d3f116aff08fa58a0c345f43
shakti <shakti.ingenioushitech@gmail.com>
2022-11-14 19:54:28 +0530

    make it better

--- controller/blogController.js
+++ controller/blogController.js
@@ -1,4 +1,4 @@
-const contactModel =require("../models/conactModel.js")
+const blogModel =require("../models/blogmodel.js")
 //Create Jobs
 module.exports.addblog = async (req, res) => {
     const { heading ,content} = req.body
@@ -18,3 +18,69 @@ module.exports.addblog = async (req, res) => {
     res.status(401).send({"status": "401","success":false, "message": "All fields are required" })
       }
     }
+//updateblog............................................................/
+module.exports.updateBlog = async (req, res) => {
+    const { heading ,content,_id} = req.body
+    try{
+    const data = await blogModel.findByIdAndUpdate({ _id: _id },
+      {
+        heading: heading,
+        content: content,
+      })
+    if(data){
+    res.send({ "status": "201","success":true, "message": "update Blog Successfully",data })
+    }else{
+      res.status(401).send({"status": "401","success":false, "message": "Unable To update" })
+    }
+    }catch(error){
+      res.status(401).send({"status": "401","success":false, "message":  "Something went Wrong" })
+      console.log("error",error);
+  }
+  }
+  //serviceBlog.............................................................//
+  module.exports.listBlog = async (req, res) => {
+    
+    try{
+    const data = await blogModel.find()
+    if(data){
+    res.send({ "status": "201","success":true, "message": "Get List  Blog Successfully",data })
+    }else{
+      res.status(401).send({"status": "401","success":false, "message": "Unable To Get" })
+    }
+    }catch(error){
+      res.status(401).send({"status": "401","success":false, "message":  "Something went Wrong" })
+      console.log("error",error);
+  }
+  }
+  //detailsBlog..............................................................................//
+  module.exports.detailsBlog = async (req, res) => {
+    const { _id }  = req.query;
+    console.log("id",_id);
+    try{
+    const data = await blogModel.findById({_id:_id})
+    if(data){
+    res.send({ "status": "201","success":true, "message": "Get  blog  Successfully",data })
+    }else{
+      res.status(401).send({"status": "401","success":false, "message": "Unable To Get" })
+    }
+    }catch(error){
+      res.status(401).send({"status": "401","success":false, "message":  "Something went Wrong" })
+      console.log("error",error);
+  }
+  }
+  //deleteBlog.....................................................................
+  module.exports.deleteBlog = async (req, res) => {
+    const  _id  = req.body._id
+    console.log("id",_id);
+    try{
+    const data = await blogModel.findByIdAndDelete({_id:_id})
+    if(data){
+    res.send({ "status": "201","success":true, "message": "Delete log Successfully",data })
+    }else{
+      res.status(401).send({"status": "401","success":false, "message": "Unable To Get" })
+    }
+    }catch(error){
+      res.status(401).send({"status": "401","success":false, "message":  "Something went Wrong" })
+      console.log("error",error);
+  }
+  }
