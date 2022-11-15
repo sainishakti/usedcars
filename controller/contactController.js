0b77bd60f1b0f206b77883a5af3807041ca1b840
shakti <shakti.ingenioushitech@gmail.com>
2022-11-15 20:11:51 +0530

    make it better

--- controller/contactController.js
+++ controller/contactController.js
@@ -60,3 +60,18 @@ module.exports.addContact = async (req, res) => {
       console.log("error",error);
 }
   }
+  //listContact..........................................
+  module.exports.listForm = async (req, res) => {
+
+    try{
+    const data = await contactModel.find()
+    if(data){
+    res.send({ "status": "201","success":true, "message": "List Get  Contact Successfully",data })
+    }else{
+      res.status(401).send({"status": "401","success":false, "message": "Unable To Get" })
+    }
+    }catch(error){
+      res.status(401).send({"status": "401","success":false, "message":  "Something went Wrong" })
+      console.log("error",error);
+}
+  }
