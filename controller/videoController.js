d7b0ace45b5611a77ad848973e605c76655758c0
shakti <shakti.ingenioushitech@gmail.com>
2022-11-17 21:04:38 +0530

    make it better

--- controller/videoController.js
+++ controller/videoController.js
@@ -3,17 +3,22 @@ const logoModel =require("../models/logoUploadmodel.js")
 
 module.exports.addVideo = async (req, res) => {
    try {
-    // console.log("data",req.file.fileName);
-    const data = new videoModel({
+    const user = await videoModel.findOne({_id:"637523ce79e7cc90dadfc4a4"})
+console.log("user",user);
+    if (!user ){
+  const data = new videoModel({
         video: "https://adminportals.herokuapp.com/upload/"+req?.file?.filename
             })
             await data.save()
-            if(data){
-            
-                res.status(201).send({ "status":"200", "success":true, "message": "Add Video  Successfully",data})
+           res.status(201).send({ "status":"200", "success":true, "message": "Add Video  Successfully",data})
             }else{
-                res.status(401).send({ "status": "401","success":false, "message": "Unable to Add Video" })
-            }
+              const data = await videoModel.findOneAndUpdate({_id:"637523ce79e7cc90dadfc4a4"},
+              {
+                video: "https://adminportals.herokuapp.com/upload/"+req?.file?.filename
+              })
+            if(data){
+            res.send({ "status": "201","success":true, "message": "Replace Video Successfully",data })
+            }}
             } catch (error) {
             console.log(error)
             res.status(401).send({ "status": "401","success":false, "message": "Unable to Add Video" })
